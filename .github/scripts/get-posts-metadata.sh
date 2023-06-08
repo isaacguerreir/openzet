18160ebd456347617f85a99ef37e0befcf0bbb4f
isaacguerreir <isaacguerreirocom@gmail.com>
2023-06-08 19:25:11 -0300

    fix mistake because of a changed file name for paths and names file

--- .github/scripts/get-posts-metadata.sh
+++ .github/scripts/get-posts-metadata.sh
@@ -1,8 +1,8 @@
 posts=$GITHUB_WORKSPACE/cards/**/README.md 
 out_folder=$GITHUB_WORKSPACE/out
 
-postnames=$(ls $posts | xargs -I {} bash -c "head -n1 {} | cut -c3-" > $out_folder/paths) 
-metadata=$(ls $posts | xargs -I {} bash -c "git --no-pager log --format='%ci#{}#' -- '{}'" > $out_folder/names)
+postnames=$(ls $posts | xargs -I {} bash -c "head -n1 {} | cut -c3-" > $out_folder/names) 
+metadata=$(ls $posts | xargs -I {} bash -c "git --no-pager log --format='%ci#{}#' -- '{}'" > $out_folder/paths)
 
 paste -d '' $out_folder/paths $out_folder/names > $out_folder/data
 cat $out_folder/data | sort -r | awk -F'#' '{print substr($1, 0, 19) " " "[" $3 "]" "(" $2  ")"}' > $GITHUB_WORKSPACE/README.md
